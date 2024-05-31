04aac98363ff7cff8db1e1a65c24fd64de28560e
vitosnatios <vitosdeveloper@gmail.com>
2024-05-31 03:28:31 -0300

    UserInMemoryRepository

---
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -0,0 +1,23 @@
+import { InMemoryRepository } from '@/shared/domain/repositories/in-memory.repository';
+import { ConflictError } from '@/shared/errors/conflict-error';
+import { NotFoundError } from '@/shared/errors/not-found-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+
+export class UserInMemoryRepository
+  extends InMemoryRepository<UserEntity>
+  implements UserRepository
+{
+  async findByEmail(email: string): Promise<UserEntity> {
+    const entity = this.items.find((i) => i.email === email);
+    if (!entity)
+      throw new NotFoundError(`Entity using the email ${email} not found`);
+    return entity;
+  }
+
+  async emailExists(email: string): Promise<void> {
+    const entity = this.items.find((i) => i.email === email);
+    if (!entity) return;
+    throw new ConflictError(`This email address is already in use`);
+  }
+}
