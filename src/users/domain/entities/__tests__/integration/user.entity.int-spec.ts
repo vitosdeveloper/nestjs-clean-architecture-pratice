229d47f84b2647a354ccfcd3b6b7b63e3d5e7e76
vitosnatios <vitosdeveloper@gmail.com>
2024-05-30 23:22:36 -0300

    integration tests - updatePassword method

--- src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/integration/user.entity.int-spec.ts
@@ -2,18 +2,18 @@ import { EntityValidationError } from '@/shared/errors/validation-error';
 import { UserEntity, UserProps } from '../../user.entity';
 import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
 
-const verifyInvalidFields = (field: keyof UserProps) => {
-  let props: UserProps = { ...UserDataBuilder({}), name: null };
-  expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-  props = { ...props, [field]: '' };
-  expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-  props = { ...props, [field]: 1 as any };
-  expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-  props = { ...props, [field]: 'a'.repeat(256) };
-  expect(() => new UserEntity(props)).toThrow(EntityValidationError);
-};
-
 describe('UserEntity integration tests', () => {
+  const verifyInvalidFields = (field: keyof UserProps) => {
+    let props: UserProps = { ...UserDataBuilder({}), name: null };
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    props = { ...props, [field]: '' };
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    props = { ...props, [field]: 1 as any };
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+    props = { ...props, [field]: 'a'.repeat(256) };
+    expect(() => new UserEntity(props)).toThrow(EntityValidationError);
+  };
+
   describe('constructor method', () => {
     it('should throw an error when creating a user with a invalid name', () => {
       verifyInvalidFields('name');
@@ -38,16 +38,19 @@ describe('UserEntity integration tests', () => {
     });
   });
 
-  const verifyInvalidUpdateValues = (
-    field: 'updateName' | 'updatePassword',
-  ) => {
-    const entity = new UserEntity(UserDataBuilder({}));
-    expect(() => entity[field](null)).toThrow(EntityValidationError);
-    expect(() => entity[field]('')).toThrow(EntityValidationError);
-    expect(() => entity[field](1 as any)).toThrow(EntityValidationError);
-    expect(() => entity[field]('a'.repeat(256))).toThrow(EntityValidationError);
-  };
   describe('update method', () => {
+    const verifyInvalidUpdateValues = (
+      field: 'updateName' | 'updatePassword',
+    ) => {
+      const entity = new UserEntity(UserDataBuilder({}));
+      expect(() => entity[field](null)).toThrow(EntityValidationError);
+      expect(() => entity[field]('')).toThrow(EntityValidationError);
+      expect(() => entity[field](1 as any)).toThrow(EntityValidationError);
+      expect(() => entity[field]('a'.repeat(256))).toThrow(
+        EntityValidationError,
+      );
+    };
+
     it('should throw an error when updating a user with a invalid name', () => {
       verifyInvalidUpdateValues('updateName');
     });
@@ -56,6 +59,16 @@ describe('UserEntity integration tests', () => {
       expect.assertions(0);
       const entity = new UserEntity(UserDataBuilder({}));
       entity.updateName('Vitor');
+    });
+
+    it('should throw an error when updating a user with a invalid password', () => {
+      verifyInvalidUpdateValues('updatePassword');
+    });
+
+    it('should be a valid password update', () => {
+      expect.assertions(0);
+      const entity = new UserEntity(UserDataBuilder({}));
+      entity.updatePassword('banana&bacon');
     });
   });
 });
