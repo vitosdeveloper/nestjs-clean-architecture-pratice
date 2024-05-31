553a0c23d6841914ca5969e14c9296805ea041b9
vitosnatios <vitosdeveloper@gmail.com>
2024-05-31 20:58:19 -0300

    adding methods for InMemorySearchableRepository

--- src/shared/domain/repositories/in-memory-searchable-repository.ts
+++ src/shared/domain/repositories/in-memory-searchable-repository.ts
@@ -1,13 +1,40 @@
 import { Entity } from '../entities/entity';
-import { SearchableRepositoryInterface } from './searchable-repository-contracts';
+import {
+  SearchParams,
+  SearchResult,
+  SearchableRepositoryInterface,
+} from './searchable-repository-contracts';
 import { InMemoryRepository } from './in-memory-repository';
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
   implements SearchableRepositoryInterface<E, any, any>
 {
-  search(props: E): Promise<E> {
+  async search(props: SearchParams): Promise<SearchResult<E>> {
     props;
     throw new Error('Method not implemented.');
+  }
+
+  protected abstract applyFilter(
+    items: E[],
+    filter: string | null,
+  ): Promise<E[]>;
+
+  protected async applySort(
+    items: E[],
+    filter: string | null,
+    sortDir: string | null,
+  ): Promise<E[]> {
+    console.log(items, filter, sortDir);
+    throw Error();
+  }
+
+  protected async applyPagination(
+    items: E[],
+    page: SearchParams['page'],
+    perPage: SearchParams['perPage'],
+  ): Promise<E[]> {
+    console.log(items, page, perPage);
+    throw Error();
   }
 }
