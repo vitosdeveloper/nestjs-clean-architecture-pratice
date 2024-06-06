4b17c14cc038f76c924b6b760d4f67ae493bcefd
vitosnatios <vitosdeveloper@gmail.com>
2024-06-06 05:21:58 -0300

    BcryptjsHashProvider unit tests

---
+++ src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,31 @@
+import { BcryptjsHashProvider } from '../../bcryptjs-hash.provider';
+
+describe('BcryptjsHashProvider unit tests', () => {
+  let sut: BcryptjsHashProvider;
+
+  beforeEach(() => {
+    sut = new BcryptjsHashProvider();
+  });
+
+  it('should return a encrypted password from the generateHash method', async () => {
+    const password = 'test-password';
+    const hash = await sut.generateHash(password);
+    console.log(hash);
+    expect(hash).toBeDefined();
+    expect(hash).not.toBe(password);
+  });
+
+  it('should compareHash method should fail', async () => {
+    const password = 'test-password';
+    const hash = await sut.generateHash(password);
+    const result = await sut.compareHash('wrong-password', hash);
+    expect(result).toBeFalsy();
+  });
+
+  it('should compareHash method should return true', async () => {
+    const password = 'test-password';
+    const hash = await sut.generateHash(password);
+    const result = await sut.compareHash(password, hash);
+    expect(result).toBeTruthy();
+  });
+});
