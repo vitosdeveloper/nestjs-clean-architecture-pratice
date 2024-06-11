34130ae1d1c72310b194b742e09a731f03fe9ef6
vitosnatios <vitosdeveloper@gmail.com>
2024-06-11 10:54:00 -0300

    DeleteUserUseCase unit tests

---
+++ src/users/application/usecases/__tests__/unit/delete-user.usecase.spec.ts
@@ -0,0 +1,30 @@
+import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { DeleteUserUseCase } from '../../delete-user.usecase';
+
+describe('DeleteUserUseCase unit tests', () => {
+  let sut: DeleteUserUseCase.UseCase;
+  let repository: UserInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new UserInMemoryRepository();
+    sut = new DeleteUserUseCase.UseCase(repository);
+  });
+
+  it('should throw a error when it can"t find a entity', async () => {
+    await expect(() => sut.execute({ id: 'fake-id' })).rejects.toThrow(
+      new NotFoundError('Entity not found'),
+    );
+  });
+
+  it('should Delete a user', async () => {
+    const spyFindById = jest.spyOn(repository, 'delete');
+    const items = [new UserEntity(UserDataBuilder({}))];
+    repository.items = items;
+    await sut.execute({ id: items[0].id });
+    expect(spyFindById).toHaveBeenCalledTimes(1);
+    expect(repository.items).toHaveLength(0);
+  });
+});
