61fe67668e1cb523b1c5f3e9420607ed19d0fb1e
vitosnatios <vitosdeveloper@gmail.com>
2024-06-11 23:01:30 -0300

    SignInUseCase

---
+++ src/users/application/usecases/sign-in.usecase.ts
@@ -0,0 +1,38 @@
+import { UserRepository } from '@/users/domain/repositories/user.repository';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { UserOutput, UserOutputMapper } from '../dtos/user-output';
+import { IUseCase } from '@/shared/application/usecases/use-case';
+import { BadRequestError } from '@/shared/application/errors/bad-request-error';
+import { InvalidCredentialsError } from '@/shared/application/errors/invalid-credentials-error';
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SignInUseCase {
+  export type Input = {
+    email: string;
+    password: string;
+  };
+
+  export type Output = UserOutput;
+
+  export class UseCase implements IUseCase<Input, Output> {
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider,
+    ) {}
+
+    async execute(input: Input): Promise<Output> {
+      if (!input.email.trim() || !input.password.trim()) {
+        throw new BadRequestError('Input data not provided');
+      }
+      const entity = await this.userRepository.findByEmail(input.email);
+      const validPassword = await this.hashProvider.compareHash(
+        input.password,
+        entity.password,
+      );
+      if (!validPassword)
+        throw new InvalidCredentialsError('Invalid credentials');
+
+      return UserOutputMapper.toOutput(entity);
+    }
+  }
+}
