5e938387555fbde98f98c14e2602e2267ca2b316
vitosnatios <vitosdeveloper@gmail.com>
2024-06-12 01:55:00 -0300

    registering SignUpUseCase

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,35 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infrastructure/env-config/env-config.module';
 import { UsersModule } from './users/infrastructure/users.module';
+import { SignUpUseCase } from './users/application/usecases/signup.usecase';
+import { UserInMemoryRepository } from './users/infrastructure/database/in-memory/repositories/user-in-memory.repository';
+import { BcryptjsHashProvider } from './users/infrastructure/providers/hash-provider/bcryptjs-hash.provider';
+import { UserRepository } from './users/domain/repositories/user.repository';
+import { HashProvider } from './shared/application/providers/hash-provider';
 
 @Module({
   imports: [EnvConfigModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'UserRepository',
+      useClass: UserInMemoryRepository,
+    },
+    {
+      provide: 'HashProvider',
+      useClass: BcryptjsHashProvider,
+    },
+    {
+      provide: SignUpUseCase.UseCase,
+      useFactory: (
+        userRepository: UserRepository.Repository,
+        hashProvider: HashProvider,
+      ) => {
+        return new SignUpUseCase.UseCase(userRepository, hashProvider);
+      },
+      inject: ['UserRepository', 'HashProvider'],
+    },
+  ],
 })
 export class AppModule {}
