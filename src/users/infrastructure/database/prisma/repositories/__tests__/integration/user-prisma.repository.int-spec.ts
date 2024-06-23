8762b2bcbd035ceb9b4085bd3f733789538cbab7
vitosnatios <vitosdeveloper@gmail.com>
2024-06-23 02:07:48 -0300

    UserPrismaRepository.search with filter, sort and paginate

--- src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
+++ src/users/infrastructure/database/prisma/repositories/__tests__/integration/user-prisma.repository.int-spec.ts
@@ -83,6 +83,59 @@ describe('UserPrismaRepository integration tests', () => {
       searchOutput.items.forEach((item) => {
         expect(item).toBeInstanceOf(UserEntity);
       });
+
+      searchOutput.items.reverse().forEach((item, index) => {
+        expect(item.email).toBe(`test${index + 1}@mail.com`);
+      });
+    });
+
+    it('should search with filter, sort and paginate', async () => {
+      const createdAt = new Date();
+      const entities: UserEntity[] = [];
+      const arrange = ['test', 'a', 'TEST', 'b', 'TeSt'];
+      arrange.forEach((element, index) => {
+        entities.push(
+          new UserEntity({
+            ...UserDataBuilder({ name: element }),
+            createdAt: new Date(createdAt.getTime() + index),
+          }),
+        );
+      });
+
+      await prismaService.user.createMany({
+        data: entities.map((item) => item.toJSON()),
+      });
+
+      const searchOutputPage1 = await sut.search(
+        new UserRepository.SearchParams({
+          page: 1,
+          perPage: 2,
+          sort: 'name',
+          sortDir: 'asc',
+          filter: 'TEST',
+        }),
+      );
+
+      expect(searchOutputPage1.items[0].toJSON()).toMatchObject(
+        entities[0].toJSON(),
+      );
+      expect(searchOutputPage1.items[1].toJSON()).toMatchObject(
+        entities[4].toJSON(),
+      );
+
+      const searchOutputPage2 = await sut.search(
+        new UserRepository.SearchParams({
+          page: 2,
+          perPage: 2,
+          sort: 'name',
+          sortDir: 'asc',
+          filter: 'TEST',
+        }),
+      );
+
+      expect(searchOutputPage2.items[0].toJSON()).toMatchObject(
+        entities[2].toJSON(),
+      );
     });
   });
 });
