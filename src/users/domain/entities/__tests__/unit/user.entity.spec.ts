39ec28f51d82b428cb374be866b5542fdb96c53c
vitosnatios <vitosdeveloper@gmail.com>
2024-06-24 14:17:17 -0300

    UserEntity unit tests refact

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -13,32 +13,32 @@ describe('UserEntity unit tests', () => {
   });
 
   test('Constructor method', () => {
-    expect(sut.props.name).toEqual(props.name);
-    expect(sut.props.email).toEqual(props.email);
-    expect(sut.props.password).toEqual(props.password);
-    expect(sut.props.createdAt).toBeInstanceOf(Date);
+    expect(sut.name).toEqual(props.name);
+    expect(sut.email).toEqual(props.email);
+    expect(sut.password).toEqual(props.password);
+    expect(sut.createdAt).toBeInstanceOf(Date);
   });
 
   test('Getter of name field', () => {
-    expect(sut.props.name).toBeDefined();
+    expect(sut.name).toBeDefined();
     expect(sut.name).toEqual(props.name);
     expect(typeof sut.name).toBe('string');
   });
 
   test('Getter of email field', () => {
-    expect(sut.props.email).toBeDefined();
+    expect(sut.email).toBeDefined();
     expect(sut.email).toEqual(props.email);
     expect(typeof sut.email).toBe('string');
   });
 
   test('Getter of password field', () => {
-    expect(sut.props.password).toBeDefined();
+    expect(sut.password).toBeDefined();
     expect(sut.password).toEqual(props.password);
     expect(typeof sut.password).toBe('string');
   });
 
   test('Getter of createdAt field', () => {
-    expect(sut.props.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeDefined();
     expect(sut.createdAt).toEqual(props.createdAt);
     expect(sut.createdAt).toBeInstanceOf(Date);
   });
