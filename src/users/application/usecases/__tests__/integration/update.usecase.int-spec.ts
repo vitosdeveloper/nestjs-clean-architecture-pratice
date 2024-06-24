cce7e4779705138f0b6d6c17f859a9f941f138a4
vitosnatios <vitosdeveloper@gmail.com>
2024-06-24 15:16:10 -0300

    UpdateUserUseCase integration tests

---
+++ src/users/application/usecases/__tests__/integration/update.usecase.int-spec.ts
@@ -0,0 +1,49 @@
+import { DatabaseModule } from '@/shared/infrastructure/database/database.module';
+import { setupPrismaTests } from '@/shared/infrastructure/database/prisma/testing/setup-prisma-tests';
+import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaClient } from '@prisma/client';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder';
+import { UpdateUserUseCase } from '../../update-user.usecase';
+import { UserPrismaRepository } from '@/users/infrastructure/database/prisma/repositories/user-prisma-repository';
+
+describe('UpdateUserUseCase integration tests', () => {
+  const prismaService = new PrismaClient();
+  let sut: UpdateUserUseCase.UseCase;
+  let repository: UserPrismaRepository;
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    setupPrismaTests();
+    module = await Test.createTestingModule({
+      imports: [DatabaseModule.forTesting(prismaService)],
+    }).compile();
+    repository = new UserPrismaRepository(prismaService as any);
+  });
+
+  beforeEach(async () => {
+    sut = new UpdateUserUseCase.UseCase(repository);
+    await prismaService.user.deleteMany();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  it('should throw a error when entity isnt found', async () => {
+    await expect(() =>
+      sut.execute({ id: 'fakeId', name: 'fake name' }),
+    ).rejects.toThrow(new NotFoundError('User of id fakeId not found.'));
+  });
+
+  it('should update a user', async () => {
+    const entity = new UserEntity(UserDataBuilder({}));
+    await prismaService.user.create({
+      data: entity.toJSON(),
+    });
+
+    const output = await sut.execute({ id: entity._id, name: 'new name' });
+    expect(output.name).toBe('new name');
+  });
+});
