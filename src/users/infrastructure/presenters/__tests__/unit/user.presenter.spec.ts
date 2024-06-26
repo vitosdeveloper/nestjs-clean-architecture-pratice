e01254db3d00425ac63e185e17022f91da4a6402
vitosnatios <vitosdeveloper@gmail.com>
2024-06-26 09:09:38 -0300

    UserPresenter unit tests 2

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,22 +1,36 @@
+import { instanceToPlain } from 'class-transformer';
 import { UserPresenter } from '../../user.presenter';
 
 describe('UserPresenter unit tests', () => {
-  const createdAt = new Date();
+  let sut: UserPresenter;
   const props = {
     id: 'e71c52a2-9710-4a96-a08e-144af4209b5d',
     name: 'test name',
     email: 'a@a.com',
     password: 'fake',
-    createdAt,
+    createdAt: new Date(),
   };
+
+  beforeEach(() => {
+    sut = new UserPresenter(props);
+  });
 
   describe('constructor', () => {
     it('should be defined', () => {
-      const sut = new UserPresenter(props);
       expect(sut.id).toEqual(props.id);
       expect(sut.name).toEqual(props.name);
       expect(sut.email).toEqual(props.email);
       expect(sut.createdAt).toEqual(props.createdAt);
+    });
+  });
+
+  it('should present data correctly', () => {
+    const output = instanceToPlain(sut);
+    expect(output).toStrictEqual({
+      id: 'e71c52a2-9710-4a96-a08e-144af4209b5d',
+      name: 'test name',
+      email: 'a@a.com',
+      createdAt: props.createdAt.toISOString(),
     });
   });
 });
